03ee987e6b595884524027f3337566a205481c86
xxu061 <frank100009@hotmail.com>
2024-03-06 00:47:02 +1300

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+// Plugins
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-
-    open: true,
-    cors: true,
-  },
-  base: './'
+    plugins: [
+        vue()
+    ],
+    define: { 'process.env': {} },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000,
+        open: true,
+        cors: true,
+    },
+    base: './',
 })
