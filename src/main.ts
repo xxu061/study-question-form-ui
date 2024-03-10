b477776f6972ac39358a3e27bf836cd3f1d0e075
frankxubidone <frank.xu@bidone.co.nz>
2024-03-11 00:18:29 +1300

    remove jquery

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import jQuery from "jquery";
-const $ = jQuery;
-window.$ = $;
 
 const app = createApp(App)
 
